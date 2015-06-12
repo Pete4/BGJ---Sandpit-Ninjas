10ac4516e100695f8a45abc3f6fbe00f33feda0a
Simon Teague <simon_teague@live.co.uk>
2015-06-13 00:49:02 +0100

    Added Sound Folder & Started Work On Mechanics

--- client/client.js
+++ client/client.js
@@ -1,25 +1,44 @@
+//Game objects
+var players = [];
+var asteroids = [];
+var canvas = $("#game-canvas");
+var ctx = canvas.getContext("2d");
+
 $(function() {
 	$("#game-canvas").css('height', $(window).height());
 	$("#game-canvas").css('width', $(window).width());
 	
+	addPlayerToGame();
+	
+	//Start timed canvas updates for UI
+	setInterval(updateCanvas, 30);
+});
+
+function addPlayerToGame() {
+	//Set players' location to center of screen
+	var spaceship = new Image();
+	spaceship.src = '/images/FirstSpace.png';
+	
+	ctx.drawImage(spaceship,10,10);
+	
+	registerSocketHooks();
+}
+
+function registerSocketHooks() {
 	//socket.emit('name', object);
 	//socket.on('name', function to handle object);
 	
 	//Join player to game
 	var socket = io();
 	
-	//Game objects
-	var players = [];
-	
 	//Hooks for websocket
 	socket.on('players', function(p) {
-		players.push(p);
+		players = p;
 	})
-	
-	
-	//Place player on screen
-	
-	
-	
-	//Start timed canvas updates for UI
-});
+	socket.on('gamedata', function(obj) {
+		players = obj.players;
+		asteroids = obj.asteroids;
+	})
+}
+
+function 
