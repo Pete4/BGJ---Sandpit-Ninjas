d8c278c12db959b64c0d168cc7155fef30856bb0
Simon Teague <simon_teague@live.co.uk>
2015-06-13 02:40:00 +0100

    Added Basic Rotation

--- public/client.js
+++ public/client.js
@@ -3,6 +3,8 @@ var players = [];
 var asteroids = [];
 var canvas = $("#game-canvas")[0];
 var ctx = canvas.getContext("2d");
+var keyState = {};
+var curRotation = 0;
 var spaceshipStationary = new Image();
 var spaceshipLeft = new Image();
 var spaceshipRight = new Image();
@@ -12,6 +14,15 @@ spaceshipLeft.src = 'images/FirstSpace_RightFlame.png';
 spaceshipRight.src = 'images/FirstSpace_LeftFlame.png';
 spaceshipForward.src = 'images/FirstSpace.png';
 
+//Utilities
+KEY_CODES = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+}
+var TO_RADIANS = Math.PI/180; 
+
 $(function() {
 	canvas.height = $(window).height();
 	canvas.width = $(window).width();
@@ -19,7 +30,14 @@ $(function() {
 	//registerSocketHooks();
 	
 	//Start timed canvas updates for UI
-	setInterval(updateCanvas, 30);
+	setInterval(updateCanvas, 25);
+	
+	window.addEventListener('keydown',function(e){
+		keyState[e.keyCode || e.which] = true;
+    },true);
+    window.addEventListener('keyup',function(e){
+		keyState[e.keyCode || e.which] = false;
+    },true);
 });
 
 function registerSocketHooks() {
@@ -40,8 +58,34 @@ function registerSocketHooks() {
 }
 
 function updateCanvas() {
-	//Draw spaceship based on movement
-	ctx.drawImage(spaceshipStationary, canvas.width/2-32, canvas.height/2-32);
-	//Set rotation if applicable
+	var curSpaceship = spaceshipStationary;
 	
+	//Clear canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	//Work out rotation
+	if (keyState[KEY_CODES.LEFT]) {
+		curSpaceship = spaceshipLeft;
+		if (curRotation == 0) {
+			curRotation = 356;
+		} else {
+			curRotation = curRotation - 4;
+		}
+	} else if (keyState[KEY_CODES.RIGHT]) {
+		curSpaceship = spaceshipRight;
+		if (curRotation == 359) {
+			curRotation = 3;
+		} else {
+			curRotation = curRotation + 4;
+		}
+	} else if (keyState[KEY_CODES.UP]) {
+		curSpaceship = spaceshipForward;
+	}
+	
+	//Draw spaceship
+    ctx.save();
+    ctx.translate(canvas.width/2,canvas.height/2);
+    ctx.rotate(curRotation*TO_RADIANS);
+	ctx.drawImage(curSpaceship, -32, -32);
+    ctx.restore();
 }
